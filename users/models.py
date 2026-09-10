44fd63bce1fa8afd9857cf2970f03e7d34f926cd
Auto Commit <devnull@example.com>
2026-09-10 22:57:49 +0300

    user

--- users/models.py
+++ users/models.py
@@ -5,4 +5,5 @@ class User(AbstractUser):
     pass
 
     def __str__(self):
-        return self.username
+        full_name = f"{self.first_name} {self.last_name}".strip()
+        return full_name if full_name else self.username
