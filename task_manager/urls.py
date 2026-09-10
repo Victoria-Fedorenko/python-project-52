6ff865b39ae03dabaacd7d4f5ed2520a62168b56
Auto Commit <devnull@example.com>
2026-09-10 20:22:21 +0300

    added additional urls for login and logout to task_manager urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.conf.urls import include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,6 @@ urlpatterns = [
     path('statuses/', include('statuses.urls')),
     path('tasks/', include('tasks.urls')),
     path('labels/', include('labels.urls')),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
